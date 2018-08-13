d77671560ad15010262a879ca1c78036f17d5a7a
chetanneharkar <chetuneharkar@gmail.com>
2018-08-14 00:54:51 +0530

    commit cg

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,17 @@
-import { Component } from '@angular/core';
-
+import { Component,OnInit } from '@angular/core';
+declare var jQuery: any;
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'app';
+  constructor() { }
+  ngOnInit() {
+    jQuery('#menu-toggle').click(function(e) {
+      e.preventDefault();
+      jQuery("#wrapper").toggleClass("active");
+    });
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,13 +2,50 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { RouterModule, Routes } from '@angular/router';
+import { LoginComponent } from './login/login.component';
+import { HomeComponent } from './home/home.component';
+import { StockComponent } from './stock/stock.component';
+import { SettingComponent } from './setting/setting.component';
+import { FormsModule } from '@angular/forms';
 
+// const appRoutes: Routes = [
+//   // { path:'home', component: HomeComponent}
+// ];
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LoginComponent,
+    HomeComponent,
+    StockComponent,
+    SettingComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    FormsModule,
+    
+    RouterModule.forRoot([
+      {
+         path: '',
+         component: LoginComponent
+      },
+      {
+        path: 'login',
+        component: LoginComponent
+      },
+      {
+        path: 'home',
+        component: HomeComponent
+      },
+      {
+        path: 'stock',
+        component: StockComponent
+      },
+      {
+        path: 'setting',
+        component: SettingComponent
+      }
+   ])
   ],
   providers: [],
   bootstrap: [AppComponent]
